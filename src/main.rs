2ebb2e261bea39d59fdeaa83b6a0c4d309b07acb
Thomas Lindauer <thomaslindauer@Administrators-MacBook-Pro.local>
2019-01-17 00:39:41 +0100

    mode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod average {
 
+    use std::collections::HashMap;
+
     // average value
     pub fn mean(irregular: &Vec<u32>) -> u32 {
 
@@ -31,17 +33,43 @@ mod average {
     }
 
     // value that occurs most often
-    pub fn mode(_irregular: &Vec<i32>){
+    pub fn mode(irregular: &Vec<u32>) -> u32 {
+
+        let mut keys = irregular.to_vec();
+
+        keys.dedup();
+
+        let mut sum: HashMap<_, _> = keys.iter().zip(vec![0; keys.len()]).collect();
+
+        // #TODO: count the values 
+        for key in irregular {
+
+            let counter = match sum.get(key) {
+
+                Some(x) => *x+1,
+
+                None => 0,
+
+            };
+
+            sum.insert(key, counter);
+
+        };
+
+        **sum.iter().max_by(|x, y| x.1.cmp(y.1)).unwrap().0
+
     }
 
 }
 
 fn main() {
 
-    let irregular = vec![15, 22, 32];
+    let irregular = vec![15, 15, 22, 22, 22, 32];
 
     println!("{}", average::mean(&irregular));
 
-    println!("{}", average::median(&irregular)); 
+    println!("{}", average::median(&irregular));
+
+    println!("{}", average::mode(&irregular)); 
 
 }
